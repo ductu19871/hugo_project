416bc6ff7b7b3752950b145a16eac3520c05de23
Duc Tu <tund@sonix.vn>
2023-08-21 00:16:28 +0700

    2801-2

--- custom_addons/hugocustom/__manifest__.py
+++ custom_addons/hugocustom/__manifest__.py
@@ -14,6 +14,7 @@
         "data/stock_data.xml",
         "data/account_data.xml",
         "view/res_users_view.xml",
+        "view/sale_order_view.xml",
     ],
     "installable": True,
 }
--- custom_addons/hugocustom/models/sale_order.py
+++ custom_addons/hugocustom/models/sale_order.py
@@ -3,5 +3,14 @@ from odoo import api, fields, models
 class SO(models.Model):
     _inherit = 'sale.order'
 
+    quotation_term = fields.Integer('Hiệu lực báo giá', default=30)
+    add_state = fields.Selection([('send_request','Đã gửi yêu cầu xử lý'),
+                                  ('confirmed', 'Xác nhận đơn hàng'),
+                                  ('stocked', 'Đã xuất kho'),
+                                  ('setup','Đang lắp đặt'),
+                                  ('deliveried','Giao hàng thành công'),
+                                  ('closed','Đơn hàng đã đóng'),
+                                  ], string='Các trạng thái của đơn hàng')
+    payment_method_id = fields.Many2one('account.payment.method', 'Phương thức thanh toán', domain="[('payment_type','=','inbound')]")
     def _prepare_invoice(self):
         return super(SO, self.with_context(overwrite_user_warehouse_id = self.warehouse_id ))._prepare_invoice()
